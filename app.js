8a6f90c158a88093138b193b461c67a10782e71b
Harsh Achar <32027087+HarshAchar@users.noreply.github.com>
2022-02-19 08:03:27 -0500

    Added X and Y labels

--- app.js
+++ app.js
@@ -1,40 +1,57 @@
+"use strict";
 
-  "use strict";
-  const API_URL = "./Example_Response.json"
+const API_URL = "./Example_Response.json";
+
+async function createChart() {
+  let ResponseFromAPI = await fetch(API_URL);
+  let JSON_Res = await ResponseFromAPI.json();
+  let JSON_Data1 = JSON.parse(JSON_Res.output.data1);
+  let YAxis = JSON_Data1.map(({ Balance_at_End }) => ({ Balance_at_End }));
   
-  async function createChart(){
-    let ResponseFromAPI = await fetch(API_URL);
-    let JSON_Res = await ResponseFromAPI.json();
-    let JSON_Data1 = JSON.parse(JSON_Res.output.data1);
-    let YAxis = JSON_Data1.map(({ Balance_at_End }) => ({ Balance_at_End }));
-    YAxis = YAxis.map(a => a.Balance_at_End);
-    let XAxis = YAxis.length;
-    XAxis = Array.from({length: XAxis}, (_, index) => index + 1);
-    
-    // Define chart properties
-    const labels = XAxis; // Adding X labels
-    const data = {
-      labels: labels, // Setting values for the X-Axis
-      datasets: [
-        {
-          label: "TBD_name_label", // TBD
-          fill: true,
-          backgroundColor: "rgb(255, 99, 132, 0.2)",
-          borderColor: "rgb(255, 99, 132)",
-          data: YAxis, // Setting graph values for the Y-Axis
+  // Define X and Y values
+  YAxis = YAxis.map((a) => a.Balance_at_End);
+  let XAxis = YAxis.length;
+  XAxis = [...Array(XAxis).keys()];
+  
+  // Define chart properties
+  const labels = XAxis; // Adding X labels
+  const data = {
+    labels: labels, // Setting values for the X-Axis
+    datasets: [
+      {
+        label: "TBD_name_label", // Serves as 'title' for the chart
+        fill: true,
+        backgroundColor: "rgb(255, 99, 132, 0.2)",
+        borderColor: "rgb(255, 99, 132)",
+        data: YAxis, // Setting graph values for the Y-Axis
+        fill: true,
+      },
+    ],
+  };
+  const config = {
+    type: "line",
+    data: data,
+    options: {
+      scales: {
+        y: { // adds a label for Y axis
+          title: {
+            display: true,
+            text: "Balance",
+            color: "#ff6384",
+          },
         },
-      ],
-    };
-    const config = {
-      type: "line",
-      data: data,
-      options: {},
-    };
-    
-    // Add Chart
-    const myChart = new Chart(document.getElementById("myChart"), config);
-  
-  
-  }
-  createChart()
-  
+        x: { // adds a label for X axis
+          title: {
+            display: true,
+            text: "Years",
+            color: "#ff6384",
+          },
+        },
+      },
+    },
+  };
+
+  // Add Chart
+  const myChart = new Chart(document.getElementById("myChart"), config);
+}
+createChart();
