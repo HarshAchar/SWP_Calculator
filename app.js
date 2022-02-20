b6758a8e357226bbf5ab75f5c797bfbd181ba6f1
Harsh Achar <32027087+HarshAchar@users.noreply.github.com>
2022-02-20 10:27:34 -0500

    Updated - table rendering

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const API_URL = "./Example_Response.json";
 
-async function createChart() {
+async function createChartAndTable() {
   let ResponseFromAPI = await fetch(API_URL);
   let JSON_Res = await ResponseFromAPI.json();
   let JSON_Data1 = JSON.parse(JSON_Res.output.data);
@@ -13,7 +13,7 @@ async function createChart() {
   // Define chart properties
   const labels = XAxis; // Adding X labels
   const Init_Invest_Amount = JSON_Data1[0]["Balance at Begin"];
-  const Title_Label = `Initial Investment: ${commaFormatted(Init_Invest_Amount)}`
+  const Title_Label = `Balance with an Initial Investment of ${commaFormatted(Init_Invest_Amount)}`
   const data = {
     labels: labels, // Setting values for the X-Axis
     datasets: [
@@ -64,10 +64,54 @@ async function createChart() {
     },
   };
 
-  // Add Chart
-  const myChart = new Chart(document.getElementById("myChart"), config);
+  // Setting table data
+  let headers = ['Year', 'Month', 'Balance at Begin', 'Withdrawal', 'Interest Earned', 'Balance at End'];
+  let tableRows = JSON_Data1
+  let table = document.createElement('table');
+  let headerRow = document.createElement('tr');
+
+  // Capturing button elements
+  let btnGet = document.querySelector('button');
+  let myTable = document.querySelector('#table');
+
+  /* ADDING CHART AND TABLE */
+  // Get results on Click event
+  btnGet.addEventListener('click', () => {
+    
+    // Add Chart description
+    document.getElementById('accrBal').textContent = 'Accrued balance factoring interest rates, inflation and expenditure'
+    // Add Chart
+    const myChart = new Chart(document.getElementById("myChart"), config);
+    
+    /* Adding table */
+    // Add Table description
+    document.getElementById('yearlySpread').textContent = 'The detailed monthly spread'
+    // Adding headers
+    headers.forEach(headerText => {
+        let header = document.createElement('th');
+        let textNode = document.createTextNode(headerText);
+        header.appendChild(textNode);
+        headerRow.appendChild(header);
+    });
+    table.appendChild(headerRow);
+    // Adding rows
+    tableRows.forEach(emp => {
+        let row = document.createElement('tr');
+        Object.values(emp).forEach(text => {
+            let cell = document.createElement('td');
+            let textNode = document.createTextNode(text);
+            cell.appendChild(textNode);
+            row.appendChild(cell);
+        })
+        table.appendChild(row);
+    });
+    myTable.appendChild(table);
+    // Centering the table
+    document.querySelector("table").style.margin = "auto";
+  });
+
 }
-createChart();
+createChartAndTable();
 
 function commaFormatted(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
