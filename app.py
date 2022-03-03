7893cc9eaeaec282f6b10122f89fcfe7d0396c5a
shantanudn <shantanu.dn@gmail.com>
2022-03-04 00:40:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -82,4 +82,4 @@ async def compute_swp(request: Request, swp_amount: int = Form(...), investment:
 # =============================================================================
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(debug=True)
